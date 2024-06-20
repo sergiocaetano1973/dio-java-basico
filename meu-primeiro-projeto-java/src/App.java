722544ee0a7615ac1ca72b6a4f8c4ec7d7298d1e
sergiocaetano1973 <sercae2@gmail.com>
2024-06-20 10:18:54 -0300

    primeiro java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("boas vindas turma");
+        System.out.println("boas vindas turma dio");
     }
 }
